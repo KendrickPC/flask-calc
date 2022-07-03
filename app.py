873be0a78b41a91ba217f3e8e4f753f7cfaa00b4
KendrickPC <kennethpchang@gmail.com>
2022-07-03 15:22:43 -0700

    url parameters completed with <int:a>/<int:b>

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
-# Put your app in here.
 from flask import Flask
+from operations import add, sub, mult, div
+# Put your app in here.
+
 
 app = Flask(__name__)
+
+@app.route("/add/<int:a>/<int:b>")
+def addTwoNumbers(a, b):
+  """Add the a and b parameter's sum in the URL"""
+  sum = add(a, b)
+  return str(sum)
+
+
+@app.route("/sub/<int:a>/<int:b>")
+def subtractTwoNums(a, b):
+  """Subtract a and b parameter's difference in the URL"""
+  result = sub(a, b)
+  return str(result)
+
+@app.route("/mult/<int:a>/<int:b>")
+def productTwoNums(a, b):
+  """Returns a and b parameter's product in the URL"""
+  product = mult(a, b)
+  return str(product)
+
+@app.route("/div/<int:a>/<int:b>")
+def divTwoNums(a,b):
+  """Returns a and b parameter's product in the URL"""
+  result = div(a, b)
+  return str(result)
+
 
